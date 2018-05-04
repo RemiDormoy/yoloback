8fd4a8c5f8e7c0672f1f25ee5514970c1ae62e2e
Remi Dormoy <rdormoy@octo.com>
2018-05-04 17:29:13 +0200

    added one get and one post

--- src/main/kotlin/com.octo.remi.yoloback/repository/FootballPlayerRepository.kt
+++ src/main/kotlin/com.octo.remi.yoloback/repository/FootballPlayerRepository.kt
@@ -11,10 +11,24 @@ class FootballPlayerRepository {
 
     fun getPlayers() = players
 
+    @Throws(PlayerAlreadyExistException::class)
+    fun addPlayer(player: FootballPlayer) {
+        if (players.contains(player)) {
+            throw PlayerAlreadyExistException()
+        } else {
+            players.add(player)
+        }
+    }
+
     @PostConstruct
     fun init() {
         players.addAll(listOf(
-                FootballPlayer("Cristiano", "Ronaldo", 33, "Madrid", "Portugal")
+                FootballPlayer("Cristiano", "Ronaldo", 33, "Madrid", "Portugal"),
+                FootballPlayer("Neymar Jr", "da Silva Santos",26 , "Paris", "Brésil"),
+                FootballPlayer("Antoine", "Griezmann", 27, "Madrid", "France")
+
         ))
     }
+
+    class PlayerAlreadyExistException() : Throwable()
 }
---
+++ src/main/kotlin/com.octo.remi.yoloback/restcontroller/PlayerController.kt
@@ -0,0 +1,30 @@
+package com.octo.remi.yoloback.restcontroller
+
+import com.octo.remi.yoloback.entities.FootballPlayer
+import com.octo.remi.yoloback.repository.FootballPlayerRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class PlayerController {
+
+    @Autowired
+    private lateinit var playerRepository: FootballPlayerRepository
+
+    @RequestMapping("/footballeurs", method = [(RequestMethod.GET)])
+    fun players() = playerRepository.getPlayers()
+
+    @RequestMapping("/footballeurs", method = [(RequestMethod.POST)])
+    fun addPlayer(@RequestBody player: FootballPlayer) {
+        try {
+            playerRepository.addPlayer(player)
+        } catch (e: FootballPlayerRepository.PlayerAlreadyExistException) {
+            throw DuplicatePlayerException()
+        }
+    }
+
+}
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Ce joueur existe déjà, déso pas déso ¯\\_(ツ)_/¯")
+class DuplicatePlayerException() : RuntimeException()
