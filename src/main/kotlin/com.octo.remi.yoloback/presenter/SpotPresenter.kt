e22407da0dfde158e96ebfa1f2d0f6dac14ed2b7
Remi Dormoy <rdormoy@octo.com>
2018-05-08 21:41:17 +0200

    petit tri des familles au calme

--- src/main/kotlin/com.octo.remi.yoloback/presenter/SpotPresenter.kt
+++ src/main/kotlin/com.octo.remi.yoloback/presenter/SpotPresenter.kt
@@ -4,9 +4,10 @@ import com.octo.remi.yoloback.entities.ChargeSpotViewModel
 import com.octo.remi.yoloback.interactor.SpotInteractor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.math.BigDecimal
 
 @Component
-class SpotPresenter @Autowired constructor(private val interactor: SpotInteractor){
+class SpotPresenter @Autowired constructor(private val interactor: SpotInteractor) {
     fun getSpots(): List<ChargeSpotViewModel> {
         val spots = interactor.getSpots()
         return spots.map {
@@ -17,5 +18,21 @@ class SpotPresenter @Autowired constructor(private val interactor: SpotInteracto
                     it.address
             )
         }
+    }
+
+    fun getSpotsWithFilter(latitude: String, longitude: String): List<ChargeSpotViewModel> {
+        val long = BigDecimal(longitude)
+        val lat = BigDecimal(latitude)
+        return interactor.getSpots().sortedBy {
+            ((long - it.longitude).abs() + (lat - it.latitude).abs())
+        }.take(10).map {
+            ChargeSpotViewModel(
+                    it.label,
+                    it.latitude.toString(),
+                    it.longitude.toString(),
+                    it.address
+            )
+        }
+
     }
 }
--- src/main/kotlin/com.octo.remi.yoloback/restcontroller/ChargeSpotController.kt
+++ src/main/kotlin/com.octo.remi.yoloback/restcontroller/ChargeSpotController.kt
@@ -1,9 +1,11 @@
 package com.octo.remi.yoloback.restcontroller
 
+import com.octo.remi.yoloback.entities.ChargeSpotViewModel
 import com.octo.remi.yoloback.presenter.SpotPresenter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,5 +15,12 @@ class ChargeSpotController {
     private lateinit var presenter: SpotPresenter
 
     @GetMapping("/bornes")
-    fun spot() = presenter.getSpots()
+    fun spot(@RequestParam("lat") latitude: String?,
+             @RequestParam("long") longitude: String?): List<ChargeSpotViewModel> {
+        return if (latitude != null && longitude != null) {
+            presenter.getSpotsWithFilter(latitude, longitude)
+        } else {
+            presenter.getSpots()
+        }
+    }
 }
