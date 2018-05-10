4228e508b468f6e17393104095abb0ade2f1d287
Remi Dormoy <rdormoy@octo.com>
2018-05-10 12:20:18 +0200

    petit identification des familles

--- src/main/kotlin/com.octo.remi.yoloback/app.kt
+++ src/main/kotlin/com.octo.remi.yoloback/app.kt
@@ -1,9 +1,13 @@
 package com.octo.remi.yoloback
 
+import com.octo.remi.yoloback.interceptor.AuthenticationInterceptor
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 fun main(args: Array<String>) {
     SpringApplication.run(App::class.java, *args)
@@ -18,4 +22,11 @@ open class App {
         return "Vas y vas mourir yolo !"
     }
 
+}
+
+@Configuration
+open class AppConfig : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(AuthenticationInterceptor())
+    }
 }
---
+++ src/main/kotlin/com.octo.remi.yoloback/interceptor/AuthenticationInterceptor.kt
@@ -0,0 +1,18 @@
+package com.octo.remi.yoloback.interceptor
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AuthenticationInterceptor : HandlerInterceptorAdapter(){
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        if (request.getHeader("token") != "yolo" && request.method == "POST") throw NotLoggedException()
+        return super.preHandle(request, response, handler)
+    }
+}
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "On t'aime pas, t'es pas loggé... ¯\\_(ツ)_/¯")
+class NotLoggedException() : RuntimeException()
